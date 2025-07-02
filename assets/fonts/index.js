154d49edde4e6701bbc0809d7cbb60fbbef1d187
kashershahzad <kashershahzadprogramming@gmail.com>
2025-07-02 15:03:10 +0500

    code push

---
+++ assets/fonts/index.js
@@ -0,0 +1,33 @@
+const bold = 'Britti-Sans-Bold';
+const boldItalic = 'Britti-Sans-Bold-Italic';
+const extraBold = 'Britti-Sans-Extra-Bold';
+const extraBoldItalic = 'Britti-Sans-Extra-Bold-Italic';
+const extraLight = 'Britti-Sans-Extralight';
+const extraLightItalic = 'Britti-Sans-Extralight-Italic';
+const light = 'Britti-Sans-Light';
+const lightItalic = 'Britti-Sans-Light-Italic';
+const medium = 'Britti-Sans-Medium';
+const mediumItalic = 'Britti-Sans-Medium-Italic';
+const regular = 'Britti-Sans-Regular';
+const regularItalic = 'Britti-Sans-Regular-Italic';
+const semiBold = 'Britti-Sans-Semibold';
+const semiBoldItalic = 'Britti-Sans-Semibold-Italic';
+const variable = 'Britti-Sans-Variable';
+
+export default {
+  bold,
+  boldItalic,
+  extraBold,
+  extraBoldItalic,
+  extraLight,
+  extraLightItalic,
+  light,
+  lightItalic,
+  medium,
+  mediumItalic,
+  regular,
+  regularItalic,
+  semiBold,
+  semiBoldItalic,
+  variable,
+};
