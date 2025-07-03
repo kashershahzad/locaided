93026f792c0c08900fd6eef41234f543457a1f85
kashershahzad <kashershahzadprogramming@gmail.com>
2025-07-03 12:49:44 +0500

    code push

--- src/screens/profilesetup/Allset/index.js
+++ src/screens/profilesetup/Allset/index.js
@@ -5,8 +5,10 @@ import ImageFast from '../../../../components/ImageFast'
 import CustomText from '../../../../components/CustomText'
 import CustomButton from '../../../../components/CustomButton'
 import fonts from '../../../../assets/fonts'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Allset() {
+    const navigation = useNavigation()
     return (
         <View>
             <ImageFast source={require('../../../../assets/allset.gif')} style={styles.animation} resizeMode={'cover'} />
@@ -14,7 +16,7 @@ export default function Allset() {
                 <ImageFast source={require('../../../../assets/auth/Customtick.png')} resizeMode={'contain'} style={styles.icon} />
                 <CustomText label={'You re all set!'} fontSize={24} textAlign={'center'} fontFamily={fonts.semiBold}/>
                 <CustomText label={'Explore your city, share moments, and earn Social Score!'} fontSize={14} textAlign={'center'} marginTop={10} fontFamily={fonts.regular} color={'#525866'} />
-                <CustomButton title={'Enter Locaided'} backgroundColor={'black'} color={'white'} marginTop={170} borderRadius={16} fontFamily={fonts.semiBold} />
+                <CustomButton title={'Enter Locaided'} backgroundColor={'black'} color={'white'} marginTop={170} borderRadius={16} fontFamily={fonts.semiBold} onPress={()=>navigation.navigate('Mainstack')}/>
             </View>
         </View>
     )
